0086919feecfcab2ef531992f9a439a4cc0a26b0
dongsu <tlsehd930@gmail.com>
2024-07-05 15:02:41 +0900

    build 수정

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/newbe93.github.io/",
-  server : {
-    port : 3000
+  base: '/newbe93.github.io/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 3000
   },
   resolve: {
-    alias: [
-      { find : "@" , replacement : "/src"},
-      { find: "@styles", replacement: "/src/styles" },
-    ]
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    }
   },
-  css : {
-    preprocessorOptions : {
-      scss : {
-        additionalData : `
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
           @import "@styles/component/variables"; 
           @import "@styles/component/mixins";
         `,
       }
     }
   },
- 
 })
